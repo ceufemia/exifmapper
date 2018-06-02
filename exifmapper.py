ec2692f0f391688daaf52e0c54d2dababbfbd405
Chris E <cbeufemia@gmail.com>
2018-06-02 12:30:50 -0400

    Added ability to plot location for a single picture to a map

--- exifmapper.py
+++ exifmapper.py
@@ -10,18 +10,24 @@ def gen_coordinate_array(coor):
     degree = degree_regex.search(coor).group(2)
     minute = minute_regex.search(coor).group(2)
     second_num = second_num_regex.search(coor).group(2)
-    second_den = second_den_regex.search(coor).group(2)
-    if second_den:
-        second = str(int(second_num) / int(second_den))
-    else:
+    try:
+        second_den = second_den_regex.search(coor).group(2)
+        second = str(float(second_num) / float(second_den))
+    except:
         second = second_num
-    return[degree, minute, second]
+    final_coor = float(degree) + float(minute)/60 + float(second)/(60*60)
+    return final_coor
 
 
 def extract_exif(imgfile):
     image = open(imgfile, 'rb')
 
     tags = exifread.process_file(image)
+    lat_coor = 0
+    latref = "N"
+    long_coor = 0
+    longref = "W"
+    dt = " "
 
     for tag in tags:
         if tag == 'GPS GPSLatitude':
@@ -36,9 +42,30 @@ def extract_exif(imgfile):
             long_coor = gen_coordinate_array(lon)
         elif tag == 'Image DateTime':
             dt = str(tags[tag])
+    try:
+        if longref == 'W':
+            long_coor = long_coor*-1
+    except:
+        print "No Longitude Data provided"
+    try:
+        if latref == 'S':
+            lat_coor = lat_coor*-1
+    except:
+        print "No Latitude Data provided"
 
-    print lat_coor[0] + ' ' + lat_coor[1] + '\' ' + lat_coor[2] + '\"' + str(latref) + ', ' + long_coor[0] + ' ' + \
-          long_coor[1] + '\'' + long_coor[2] + '\"' + str(longref) + ' at ' + dt
+    return [str(lat_coor), str(long_coor), imgfile + " taken at " + dt]
 
 
-extract_exif('C:\Users\T530\Downloads\IMG_20180527_232015.jpg')
+def mark_map(lat, lon, dt):
+    import folium
+    from folium.plugins import MarkerCluster
+    map_osm = folium.Map(location=[40.752759, -73.977251], zoom_start=10)
+
+    stations = folium.FeatureGroup(name='Pictures')
+    stations.add_child(folium.Marker([lat, lon], popup=dt))
+    map_osm.add_child(stations)
+    map_osm.save('map.html')
+
+
+exif_data = extract_exif('pic1.jpg')
+mark_map(float(exif_data[0]), float(exif_data[1]), exif_data[2])
